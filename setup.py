09f3e329a9288c5955edb6f7cf1556545366898e
Jason <dimensions3@outlook.com>
2024-05-22 15:30:53 -0700

    build configuration

--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python
+import setuptools
+with open("README.md", "r", encoding = "utf-8") as fh:
+    long_description = fh.read()
 
-from distutils.core import setup
-
-setup(name='gifanimus',
-      version='0.01',
-      description='A Very Simple Animated Gif Player',
-      author='Fibo Metavinci',
-      author_email='pszdw-75nat-5227i-bha5s-y7lai-pebdq-o2agp-3xho4-hd6z6-emxrd-nqe@dmail.ai',
-      url='https://github.com/inviti8',
-      packages=['gifanimus'],
-      install_requires=['pillow==10.3.0'],
-     )
+setuptools.setup(
+    name = "gifanimus",
+    version = "0.0.1",
+    author = "Fibo Metavinci",
+    author_email = "pszdw-75nat-5227i-bha5s-y7lai-pebdq-o2agp-3xho4-hd6z6-emxrd-nqe@dmail.ai",
+    description = "A very simple gif animation player.",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/inviti8",
+    project_urls = {
+        "Bug Tracker": "https://github.com/inviti8/gifanimus/issues",
+    },
+    package_dir = {"": "gifanimus"},
+    packages = setuptools.find_packages(where="gifanimus"),
+    python_requires = ">=3.9.18"
+)
